0e780d0121ce2570849d585ace865bc2f00f7388
yugmani <yugmani@yahoo.com>
2022-04-16 07:13:31 -0700

    constructor and instances

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
 
+// class
+// **************************************
+
 //take a moment to study the class
 class Dog {
   constructor(name) {
@@ -23,11 +26,34 @@ class Dog {
 //the class instantiation, method call, and console.log()
 const halley = new Dog('Halley');
 
-console.log(halley.name);   //Halley
-console.log(halley.behavior);   //0
+console.log(halley.name); //Halley
+console.log(halley.behavior); //0
 
 //method call
 halley.incrementBehavior();
 
-console.log(halley.name);   //Halley
-console.log(halley.behavior);   //1
+console.log(halley.name); //Halley
+console.log(halley.behavior); //1
+
+// constructor
+// ********************************************
+
+//1. Create an empty class named Surgeon.
+//2. Inside the Surgeon class, create a constructor() method that accepts two parameters: name and department.
+//3. Inside the Surgeon constructor(), create name and department properties and set them equal to your input parameters.
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+//4. Create an instance of the Surgeon class — set the name to 'Francisco Romero' and department to 'Cardiovascular'.
+
+// Save the instance to a constant variable called surgeonRomero.
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+
+//5. Create an instance of the Surgeon class — set the name to 'Ruth Jackson' and department to 'Orthopedics'.
+
+// Save the instance to a constant variable called surgeonJackson.
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
